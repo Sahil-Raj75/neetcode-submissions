59ac66a207e0dcdddf326d2d46662350394ca5d3
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-07-10 14:39:12 +0530

    Add: palindrome-linked-list - submission-2

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // reverse the whole list
+        Stack<Integer> stk = new Stack<>();
+        ListNode temp = head;
+        while(temp != null){
+            stk.push(temp.val);
+            temp = temp.next;
+        }
+        // stack me Last element first out 
+        // agar fir se hum starting se traverse kare tho stack me element same hona cahiye 
+        temp = head;
+
+        while(temp != null && temp.val == stk.pop()){
+            temp = temp.next;
+        }    
+        return stk.isEmpty();
+    }
+}
