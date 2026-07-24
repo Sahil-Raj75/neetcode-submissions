18c1d5dfb73e2a02234767fce077639f8ba82dee
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-07-25 01:31:36 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    static void solve(int[] arr,int idx, List<List<Integer>> ans, List<Integer> output, int k){
+        // base-case
+        if(output.size() == k){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+        if(idx >= arr.length){
+            return;
+        }
+
+        // include the current candidate
+        // include kr de aage wale ko call kr rhe
+        output.add(arr[idx]);
+        solve(arr, idx+1, ans, output,k);
+
+        // bina include kiye next call
+        output.remove(output.size()-1);
+        // exclude
+        solve(arr, idx+1, ans, output,k);
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        int[] candidates = new int[n];
+        int pos =0;
+        for(int i = 1; i<=n; i++){
+            candidates[pos++] = i;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        int idx = 0;
+        solve(candidates, idx, ans , output ,k);
+        return ans;
+    }
+}
