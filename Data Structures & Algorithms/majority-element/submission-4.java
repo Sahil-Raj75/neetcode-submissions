a83929afe73e9f352910d8260cab8fc59eb9db68
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-08-06 00:23:43 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // randomization
+        Random random = new Random();
+
+        while(true){
+            int val = nums[random.nextInt(nums.length)];
+
+            int count = 0;
+
+            for(int num : nums){
+                if(num == val){
+                    count++;
+                }
+            }
+            if(count > nums.length /2){
+                return val;
+            }
+        }
+    }
+}
