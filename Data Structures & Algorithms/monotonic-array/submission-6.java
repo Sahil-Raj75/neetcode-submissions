58471ca11dc2e118efd486c1078b73b6545c2f8f
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-08-14 00:25:51 +0530

    Add: monotonic-array - submission-6

---
+++ Data Structures & Algorithms/monotonic-array/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            // monotone inc
+            if (nums[0] <= nums[nums.length -1]) {
+                if (nums[i] <= nums[i + 1]) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }else{
+                if (nums[i] >= nums[i + 1]) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
