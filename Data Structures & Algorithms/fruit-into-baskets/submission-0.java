e06cb390cb98ef7f354da71849a8d49c55b221ab
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-08-18 19:08:33 +0530

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int low = 0;
+        int res = 0;
+        int n = fruits.length;
+
+        for (int high = 0; high < n; high++) {
+            int fruit = fruits[high];
+            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+
+            while (map.size() > 2) {
+                int fruitAtLow = fruits[low];
+                map.put(fruitAtLow, map.get(fruitAtLow) - 1);
+                if (map.get(fruitAtLow) == 0) {
+                    map.remove(fruitAtLow);
+                }
+                low++;
+            }
+
+            if (map.size() == 2) {
+                int len = high - low + 1;
+                res = Math.max(res, len);
+            }
+        }
+        if (map.size() < 2) {
+            return fruits.length;
+        }
+        return res;
+    }
+}
