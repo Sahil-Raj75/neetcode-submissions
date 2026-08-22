f1d989a52bf4989dae2779abe140085637368568
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-08-23 00:07:06 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+
+        for(char ch1 : t.toCharArray()){
+            // if(freq[ch1 - 'a'] == 0){
+            //     return false;
+            // }
+            freq[ch1 - 'a']--;
+        }
+
+        for(int count: freq){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
