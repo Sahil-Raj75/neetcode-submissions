db94ba80221146add8b88cada017f5ade6386bd2
Sahil <164072302+Sahil-Raj75@users.noreply.github.com>
2026-08-23 00:01:00 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch1 : t.toCharArray()) {
+            if (!map.containsKey(ch1)) {
+                return false;
+            }
+            map.put(ch1, map.getOrDefault(ch1, 0) - 1);
+            if (map.get(ch1) == 0) {
+                map.remove(ch1);
+            }
+        }
+        return map.isEmpty() ? true : false;
+    }
+}
